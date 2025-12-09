c3b0536dd568dd2558eafe29b586820176c52fe0
Johan Moszkowicz <advocaatjohand@gmail.com>
2025-12-09 01:57:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("OK"));
-app.head("/", (req, res) => res.status(200).end());
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Keep-alive server running");
-});
-
 require('dotenv').config();
-
+const express = require("express");
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 
-// --- START EXPRESS SERVER DIRECT ---
-const express = require("express");
+// --- EXPRESS HEALTHCHECK SERVER START DIRECT ---
 const app = express();
-
 app.get("/", (req, res) => res.status(200).send("OK"));
+app.head("/", (req, res) => res.status(200).end());
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Healthcheck server running on port " + (process.env.PORT || 3000));
+    console.log("Healthcheck server running on port " + (process.env.PORT || 3000));
 });
 
 // --- DISCORD CLIENT ---
@@ -31,21 +20,21 @@ const client = new Client({
 client.commands = new Collection();
 client.modals = new Collection();
 
-// --- Commands ---
+// --- Load commands ---
 const commandsPath = path.join(__dirname, 'commands');
 for (const file of fs.readdirSync(commandsPath)) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
 }
 
-// --- Modals ---
+// --- Load modals ---
 const modalsPath = path.join(__dirname, 'modals');
 for (const file of fs.readdirSync(modalsPath)) {
     const modal = require(`./modals/${file}`);
     client.modals.set(modal.customId, modal);
 }
 
-// --- Events ---
+// --- Load events ---
 const eventsPath = path.join(__dirname, 'events');
 for (const file of fs.readdirSync(eventsPath)) {
     const event = require(`./events/${file}`);
@@ -57,4 +46,3 @@ for (const file of fs.readdirSync(eventsPath)) {
 }
 
 client.login(process.env.TOKEN);
-
